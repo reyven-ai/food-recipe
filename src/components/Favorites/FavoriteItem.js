8da22da34b57b939af9abaca1b9e31b17913ce9f
Reyven Villaester <reyvenvillaester26@gmail.com>
2023-12-28 16:55:55 +0200

    Updated FavoriteItem.js file

--- src/components/Favorites/FavoriteItem.js
+++ src/components/Favorites/FavoriteItem.js
@@ -1,25 +1,15 @@
-import React from "react";
-
 import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-const FavoriteImage = styled.img`
-  width: 100%;
-  height: 200px;
-  object-fit: cover;
-  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.1);
-  border-radius: 10px;
-
-  @media (max-width: 576px) {
-    height: auto;
-`;
 
 const FavoriteItem = ({ idMeal, strMeal, strMealThumb }) => {
   return (
     <div>
       <li key={idMeal}>
-        <Link to={`/movie/${idMeal}`}>
-          <FavoriteImage src={strMealThumb} alt={strMeal} />
+        <Link to={`/view/${idMeal}`}>
+          <img
+            className="w-full h-[200px] object-cover shadow-md rounded-lg"
+            src={strMealThumb}
+            alt={strMeal}
+          />
         </Link>
       </li>
     </div>
